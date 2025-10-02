fb35e6f4f98a06a91226ed79df7629940674a3ee
Mustafa Saied <mustafa.saied@astc.com.sa>
2025-10-02 14:31:22 +0300

    edit

--- app/pages/(home)/index.vue
+++ app/pages/(home)/index.vue
@@ -1,26 +1,20 @@
 <template>
   <div class="">
     <HeroSection />
-    <component
+    <!-- <component
       v-for="section in sections"
       :key="section.id"
       :is="componentMap[section.code] || null"
       :section-data="section"
-    />
+    /> -->
   </div>
 </template>
 
 <script setup lang="ts">
-import type { SectionCode } from '~/services/pages-section.service'
+// import type { SectionCode } from '~/services/pages-section.service'
 
 import HeroSection from '~/pages/(home)/_components/hero-section/HeroSection.vue'
-import NewsSection from '~/pages/(home)/_components/NewsSection.vue'
-import AnnouncementSection from '~/pages/(home)/_components/AnnouncementSection.vue'
-import PromoSection from '~/pages/(home)/_components/PromoSection.vue'
-import CustomersSection from '~/pages/(home)/_components/CustomersSection.vue'
-import ParallaxSection from '~/pages/(home)/_components/parallax-section/ParallaxSection.vue'
-import PartnersSection from '~/pages/(home)/_components/PartnersSection.vue'
-import SaudiVisionSection from '~/pages/(home)/_components/SaudiVisionSection.vue'
+
 definePageMeta({
   layoutProps: {
     showLeftLogo: false,
@@ -29,14 +23,14 @@ definePageMeta({
 })
 usePageHead()
 
-const componentMap: Partial<Record<SectionCode, any>> = {
-  news_bar: NewsSection,
-  vision: AnnouncementSection,
-  custom_development: PromoSection,
-  trusted_customers: CustomersSection,
-  digital_cloud: ParallaxSection,
-  trusted_partners: PartnersSection,
-  vision2030: SaudiVisionSection
-}
+// const componentMap: Partial<Record<SectionCode, any>> = {
+//   news_bar: NewsSection,
+//   vision: AnnouncementSection,
+//   custom_development: PromoSection,
+//   trusted_customers: CustomersSection,
+//   digital_cloud: ParallaxSection,
+//   trusted_partners: PartnersSection,
+//   vision2030: SaudiVisionSection
+// }
 const { data: sections } = await useApiAsyncData(() => getPageSection('home'))
 </script>
