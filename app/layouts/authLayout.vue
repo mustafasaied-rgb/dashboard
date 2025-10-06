6b21b4900c1322f24bab2a7bacd68918e14982d6
Mustafa Saied <mustafa.saied@astc.com.sa>
2025-10-06 19:12:03 +0300

    edit

---
+++ app/layouts/authLayout.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex h-screen items-stretch justify-between">
+    <div class="flex-1">
+      <slot />
+    </div>
+    <div class="h-full w-1/3 bg-black/20"></div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
