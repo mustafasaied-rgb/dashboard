c7a7c401c8e67d7dd965675882a34c750991ec75
Mustafa Saied <mustafa.saied@astc.com.sa>
2025-10-10 16:02:01 +0300

    alerts

--- app/components/ui/Alert.vue
+++ app/components/ui/Alert.vue
@@ -15,7 +15,7 @@
         <router-link
           v-if="showLink"
           :to="linkHref"
-          class="inline-block mt-3 text-sm font-medium text-gray-500 underline dark:text-gray-400"
+          class="mt-3 inline-block text-sm font-medium text-gray-500 underline dark:text-gray-400"
         >
           {{ linkText }}
         </router-link>
@@ -25,8 +25,7 @@
 </template>
 
 <script setup lang="ts">
-import { SuccessIcon, ErrorIcon, WarningIcon, InfoCircleIcon } from '@/icons'
-import { computed } from 'vue'
+import { SuccessIcon, ErrorIcon, WarningIcon, InfoCircleIcon } from '~/components/icons'
 
 interface AlertProps {
   variant: 'success' | 'error' | 'warning' | 'info'
@@ -40,33 +39,33 @@ interface AlertProps {
 const props = withDefaults(defineProps<AlertProps>(), {
   showLink: false,
   linkHref: '#',
-  linkText: 'Learn more',
+  linkText: 'Learn more'
 })
 
 const variantClasses = {
   success: {
     container: 'border-success-500 bg-success-50 dark:border-success-500/30 dark:bg-success-500/15',
-    icon: 'text-success-500',
+    icon: 'text-success-500'
   },
   error: {
     container: 'border-error-500 bg-error-50 dark:border-error-500/30 dark:bg-error-500/15',
-    icon: 'text-error-500',
+    icon: 'text-error-500'
   },
   warning: {
     container: 'border-warning-500 bg-warning-50 dark:border-warning-500/30 dark:bg-warning-500/15',
-    icon: 'text-warning-500',
+    icon: 'text-warning-500'
   },
   info: {
     container:
       'border-blue-light-500 bg-blue-light-50 dark:border-blue-light-500/30 dark:bg-blue-light-500/15',
-    icon: 'text-blue-light-500',
-  },
+    icon: 'text-blue-light-500'
+  }
 }
 
 const icons = {
   success: SuccessIcon,
   error: ErrorIcon,
   warning: WarningIcon,
-  info: InfoCircleIcon,
+  info: InfoCircleIcon
 }
 </script>
