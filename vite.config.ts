edcacea75ae33754b3b0e2db03c16677d9fa514e
Mustafa Saied <mustafasaied97@gmail.com>
2025-10-22 11:23:01 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,16 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  server: {
+    allowedHosts: [
+      'skjrx7-5173.csb.app', // 👈 add your host here
+      'localhost',
+    ],
   },
 })
